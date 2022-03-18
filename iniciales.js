95d5651e858b8355b578be777e3aea9684cd5d57
chenaoh <emiliaserrano2814@gmail.com>
2022-03-18 18:01:19 -0400

    modifique el documento no se veia

--- iniciales.js
+++ iniciales.js
@@ -9,12 +9,3 @@ let secondInitial = name.slice(secondInitialPosition,secondInitialPosition+1);
 // console.log(firstInitial+secondInitial);
 //mostrando en la web
 document.write(`Las iniciales de tu nombre y apellido son ${firstInitial.toUpperCase()}${secondInitial.toUpperCase()}`);
-
-
-const restaurantBill = (50) => {
-    const tax = (50*0.1)
-
-    /* ??? */
-  
-    return /* ??? */
-  };
